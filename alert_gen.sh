969dd7b699b723c8df0a9689f07758ef10c2825a
Brian Sak <briansak@users.noreply.github.com>
2022-06-08 11:50:56 -0500

    Update alert_gen.sh

--- alert_gen.sh
+++ alert_gen.sh
@@ -3,8 +3,8 @@
 set -x 
 
 apiID=3
-apiclarity="${UPSTREAM_TELEMETRY_HOST_NAME:-localhost}:80"
-telemetry="${UPSTREAM_TELEMETRY_HOST_NAME:-localhost}:9090"
+apiclarity="localhost:80"
+telemetry="localhost:9090"
 #apiclarity="${UPSTREAM_TELEMETRY_HOST_NAME:-apiclarity-apiclarity.apiclarity:8080}"
 #telemetry="${UPSTREAM_TELEMETRY_HOST_NAME:-apiclarity-apiclarity.apiclarity:9000}"
 
